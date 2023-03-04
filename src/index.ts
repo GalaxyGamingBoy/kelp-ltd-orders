f830393cfe8d5b704bf37953371255ed6eac99d0
GalaxyGamingBoy <xrteach@gmail.com>
2023-03-04 16:37:12 +0200

    feat: Add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as dotenv from 'dotenv'
 import express from 'express';
 import Routes from './routes/Routes';
+import cors from 'cors'
 
 // Init
 dotenv.config();
 const server = express();
 const routes = new Routes(server, undefined);
+server.use(cors());
 
 // Routes
 routes.initRoutes();
